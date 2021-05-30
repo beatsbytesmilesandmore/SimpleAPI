639c88db44de0188d92be302e05180e99086da6f
Joshua B. Turner <beatsbytesmilesandmore@gmail.com>
2021-05-29 21:43:39 -0500

    Initial Commit

---
+++ test/SimpleAPI.TEST/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+// using Microsoft.AspNetCore.Mvc.Core;
+
+namespace SimpleAPI.TEST
+{
+    public class UnitTest1
+    {
+
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Something else",returnValue.Value);
+        }
+        // [Fact]
+        // public void Test1()
+        // {
+
+        // }
+    }
+}
